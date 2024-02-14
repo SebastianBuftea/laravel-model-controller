546cae3b72597cea63628d161433609b2b2d0496
SebastianBuftea <sebibuftea85@gmail.com>
2024-02-14 18:53:30 +0100

    [ADD] film from database in welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,16 +2,22 @@
 @section('content')
 
     <div class="container">
-        <div class="row">
-            <div class="col-3">
-                <div class="card">
-                    <img src="img_avatar.png" alt="Avatar" style="width:100%">
-                    <div class="container">
-                        <h4><b><b></h4>
-                        <p></p>
+        <div class="row d-flex">
+
+            @foreach ($movies as $movie)
+                <div class="col-3 my-2 ">
+                    <div class="card_">
+                        <div class="container_">
+                            <h4>Title:<b>{{ $movie['title'] }}</b></h4>
+                            <h5>Original Title: {{ $movie['original_title'] }}</h5>
+                            <h5>Nationality:{{ $movie['nationality'] }}</h5>
+                            <h5>Date: {{ $movie['date'] }}</h5>
+                            <h5>Vote: {{ $movie['vote'] }}</h5>
+                        </div>
                     </div>
                 </div>
-            </div>
+            @endforeach
+
         </div>
 
     </div>
